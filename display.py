b7c1b1cb7c8d279337c834d51c2a7c6bbedaac38
btieszen <briantieszen4@gmail.com>
2024-08-01 08:05:00 -0700

    commit1

--- display.py
+++
@@ -1,7 +0,0 @@
-# Function to display the weather forecast for a city
-
-
-def get_detailed_forecast(city):
-    # Function to provide a detailed weather forecast for a city
-    data = fetch_weather_data(city)
-    return parse_weather_data(data)
